75cd40287a11d5a561f8fbb0f125c09362b70914
nm-z <138162007+nm-z@users.noreply.github.com>
2024-07-04 22:17:37 -0700

    fixed default run command (vs code)

--- run_PCBcoilV2.bat
+++ run_PCBcoilV2.bat
@@ -1,4 +1,4 @@
 @echo off
 call C:\Users\natez\PCBcoilGenerator\venv\Scripts\activate.bat
-python c:\Users\natez\PCBcoilGenerator\PCBcoilV2.py
+C:\Users\natez\PCBcoilGenerator\venv\Scripts\python.exe c:\Users\natez\PCBcoilGenerator\PCBcoilV2.py
 pause
